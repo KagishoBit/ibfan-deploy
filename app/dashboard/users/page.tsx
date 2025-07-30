bc9c0fc26f028302919f72d78b802f59563adcd2
KagishoBit <matshusimkb@gmail.com>
2025-07-30 19:41:58 +0200

    Fixing auth 2

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,6 +1,8 @@
 import { getUsers, addUser, updateUser, deleteUser } from '@/app/lib/actions';
 import UserManagementClient from './UserManagementClient';
 
+
+export const dynamic = 'force-dynamic'; // Add this line
 // This is now a Server Component
 export default async function UserManagementPage() {
   // Fetch initial data on the server
--- app/dashboard/violations/page.tsx
+++ app/dashboard/violations/page.tsx
@@ -1,6 +1,7 @@
 import { getViolations, addViolation, updateViolation, deleteViolation } from '@/app/lib/actions';
 import { ViolationsClient } from './ViolationsClient';
 
+export const dynamic = 'force-dynamic'; // Add this line
 export default async function ViolationsPage() {
   const violations = await getViolations();
 
